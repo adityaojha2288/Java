bb621e8bd2150ea4101b6efc07591b98873bee46
Aditya Ojha <adityaojha6160@gmail.com>
2023-05-06 13:36:56 +0530

    Add files via upload

---
+++ src/com/company/checkbox_menu.java
@@ -0,0 +1,46 @@
+package com.company;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import javax.swing.AbstractButton;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+    public class checkbox_menu {
+        public static void main(final String[] args) {
+            JFrame frame = new JFrame("Jmenu Example");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            JMenuBar menuBar = new JMenuBar();
+            // File Menu, F - Mnemonic
+            JMenu fileMenu = new JMenu("File");
+            fileMenu.setMnemonic(KeyEvent.VK_0);
+            menuBar.add(fileMenu);
+            // File->New, N - Mnemonic
+            JMenuItem menuItem1 = new JMenuItem("Open", KeyEvent.VK_N);
+            fileMenu.add(menuItem1);
+
+            JCheckBoxMenuItem caseMenuItem = new JCheckBoxMenuItem("Option_1");
+            caseMenuItem.setMnemonic(KeyEvent.VK_C);
+            fileMenu.add(caseMenuItem);
+
+            ActionListener aListener = event -> {
+                AbstractButton aButton = (AbstractButton) event.getSource();
+                boolean selected = aButton.getModel().isSelected();
+                String newLabel;
+                if (selected) {
+                    newLabel = "Value-1";
+                } else {
+                    newLabel = "Value-2";
+                }
+                aButton.setText(newLabel);
+            };
+
+            caseMenuItem.addActionListener(aListener);
+            frame.setJMenuBar(menuBar);
+            frame.setSize(350, 250);
+            frame.setVisible(true);
+        }
+    }
+
+
