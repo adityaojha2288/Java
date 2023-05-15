216466278fdbfb44f09bd879b9637c2ff48df64d
Aditya Ojha <adityaojha6160@gmail.com>
2023-05-15 12:32:56 +0530

    Add files via upload

---
+++ src/com/company/Missing_number.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Missing_number {
+    class Solution {
+        public int missingNumber(int[] nums) {
+            Arrays.sort(nums);
+            int start =0; int end = nums.length - 1;
+            while(start <= end){
+                int mid = start + (end - start) / 2;
+                if(nums[mid] > mid){
+                    end = mid - 1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+            return start;
+        }
+    }
+}
